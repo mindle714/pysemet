d973b66ca58439648677c5076747e5df359f3e68
mindle714 <mindle714@naver.com>
2022-08-15 04:42:33 +0900

    add mapped pesq mos -> raw pesq on narrow band to follow original CSIG etc. values

--- composite.py
+++ composite.py
@@ -201,6 +201,8 @@ def snr(clean_speech, processed_speech, sr):
 
   return segmental_snr 
 
+import pesq
+
 def composite(data1, data2, sr, alpha=0.95):
   _len = min(data1.shape[0], data2.shape[0])
   data1 = data1[:_len] + eps
@@ -217,7 +219,12 @@ def composite(data1, data2, sr, alpha=0.95):
   segsnr_dist = snr(data1, data2, sr)
   segSNR = np.mean(segsnr_dist)
 
-  pesq_mos = 0
+  sr_mod = 'wb' if sr == 16000 else 'nb'
+  # TODO pesq value slightly differ from MATLAB version; need to check
+  pesq_mos = pesq.pesq(sr, data1, data2, mode=sr_mod)
+  # convert to raw pesq on narrow-band case
+  if sr_mod == 'nb':
+    pesq_mos = (math.log((4./(pesq_mos - 0.999))-1.)-4.6607) / -1.4945
 
   Csig = 3.093 - 1.029*llr_mean + 0.603*pesq_mos-0.009*wss_dist
   Cbak = 1.634 + 0.478 *pesq_mos - 0.007*wss_dist + 0.063*segSNR
--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 import composite
 import soundfile
+import pesq
+import math
 
 data1, sr1 = soundfile.read("sp09.wav")
 data2, sr2 = soundfile.read("enhanced_logmmse.wav")
 assert sr1 == sr2
+print(sr1)
+
+sr_mod = 'wb' if sr1 == 16000 else 'nb'
+pesq_mos = pesq.pesq(sr1, data1, data2, mode=sr_mod)
+# convert to raw pesq on narrow-band case
+if sr_mod == 'nb':
+  pesq_mos = (math.log((4./(pesq_mos - 0.999))-1.)-4.6607) / -1.4945
+print(pesq_mos)
 
 csig, cbak, covl, segsnr = composite.composite(data1, data2, sr1)
 print(csig, cbak, covl, segsnr)
